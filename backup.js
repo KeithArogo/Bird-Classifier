00117569d9b23ce8d53af1b3a224b3a5eb28dc45
Keith Arogo <Keithowino97@gmail.com>
2022-01-09 17:12:14 +0000

    First commit

---
+++ backup.js
@@ -0,0 +1,37 @@
+let mobilenet;
+let puffin;
+
+function modelReady() {
+    console.log('Model is ready!!');
+    mobilenet.predict(puffin, gotResults);
+}
+
+function gotResults(error, results){
+    if (error){
+        console.error(error);
+    } else {
+    console.log(results);
+    let label = results[0].className;
+    let prob = results[0].probability;
+    fill(1000);
+    textSize(32);
+    text(label, 10, height - 100);
+    createP(label);
+    createP(prob);
+    }
+
+}
+
+function imageReady () {
+    image(puffin, 0, 0, width, height);
+}
+
+function setup () {
+    createCanvas(640,480);
+    puffin = createImg('Images/penguin.jpg', imageReady);
+    puffin.hide();
+    background(0);
+    mobilenet = ml5.imageClassifier('MobileNet', modelReady);
+
+
+}
